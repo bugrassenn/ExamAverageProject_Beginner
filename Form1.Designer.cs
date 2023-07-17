bb1d3d03cd98bb482d1d1c868257a51d3c770184
Bugra Sen <bugra.sen@bil.omu.edu.tr>
2023-07-17 04:44:24 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace ExamAverageProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtExam1 = new System.Windows.Forms.TextBox();
+            this.TxtExam2 = new System.Windows.Forms.TextBox();
+            this.TxtExam3 = new System.Windows.Forms.TextBox();
+            this.BtnCalculate = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Exam = new System.Windows.Forms.GroupBox();
+            this.TxtStatus = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1.SuspendLayout();
+            this.Exam.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Exam 1 : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Exam 2 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Exam 3 :";
+            // 
+            // TxtExam1
+            // 
+            this.TxtExam1.Location = new System.Drawing.Point(138, 40);
+            this.TxtExam1.Name = "TxtExam1";
+            this.TxtExam1.Size = new System.Drawing.Size(100, 24);
+            this.TxtExam1.TabIndex = 3;
+            // 
+            // TxtExam2
+            // 
+            this.TxtExam2.Location = new System.Drawing.Point(138, 76);
+            this.TxtExam2.Name = "TxtExam2";
+            this.TxtExam2.Size = new System.Drawing.Size(100, 24);
+            this.TxtExam2.TabIndex = 4;
+            // 
+            // TxtExam3
+            // 
+            this.TxtExam3.Location = new System.Drawing.Point(138, 114);
+            this.TxtExam3.Name = "TxtExam3";
+            this.TxtExam3.Size = new System.Drawing.Size(100, 24);
+            this.TxtExam3.TabIndex = 5;
+            // 
+            // BtnCalculate
+            // 
+            this.BtnCalculate.Location = new System.Drawing.Point(138, 166);
+            this.BtnCalculate.Name = "BtnCalculate";
+            this.BtnCalculate.Size = new System.Drawing.Size(100, 34);
+            this.BtnCalculate.TabIndex = 6;
+            this.BtnCalculate.Text = "Calculate";
+            this.BtnCalculate.UseVisualStyleBackColor = true;
+            this.BtnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Status : ";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(742, 100);
+            this.panel1.TabIndex = 9;
+            // 
+            // Exam
+            // 
+            this.Exam.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.Exam.Controls.Add(this.label1);
+            this.Exam.Controls.Add(this.label2);
+            this.Exam.Controls.Add(this.label3);
+            this.Exam.Controls.Add(this.TxtExam1);
+            this.Exam.Controls.Add(this.BtnCalculate);
+            this.Exam.Controls.Add(this.TxtExam2);
+            this.Exam.Controls.Add(this.TxtExam3);
+            this.Exam.Location = new System.Drawing.Point(41, 136);
+            this.Exam.Name = "Exam";
+            this.Exam.Size = new System.Drawing.Size(278, 259);
+            this.Exam.TabIndex = 10;
+            this.Exam.TabStop = false;
+            this.Exam.Text = "Exam";
+            // 
+            // TxtStatus
+            // 
+            this.TxtStatus.Enabled = false;
+            this.TxtStatus.Location = new System.Drawing.Point(118, 56);
+            this.TxtStatus.Name = "TxtStatus";
+            this.TxtStatus.Size = new System.Drawing.Size(100, 24);
+            this.TxtStatus.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MV Boli", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(190, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(359, 49);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Average Calculator";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.TxtStatus);
+            this.groupBox1.Location = new System.Drawing.Point(379, 194);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(258, 142);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Average";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(738, 439);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Exam);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.Exam.ResumeLayout(false);
+            this.Exam.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtExam1;
+        private System.Windows.Forms.TextBox TxtExam2;
+        private System.Windows.Forms.TextBox TxtExam3;
+        private System.Windows.Forms.Button BtnCalculate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox Exam;
+        private System.Windows.Forms.TextBox TxtStatus;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamAverageProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            int exam1, exam2, exam3, average;
+            string status;
+
+            exam1 = Convert.ToInt16(TxtExam1.Text);
+            exam2 = Convert.ToInt16(TxtExam2.Text);
+            exam3 = Convert.ToInt16(TxtExam3.Text);
+
+            average = (exam1 * 40 / 100) + (exam2 * 30 / 100) + (exam3 * 30 / 100);
+            
+            if ( average >= 50 )
+            {
+               status = "Success";
+            }
+            else
+            {
+                status = "Failed";
+            }
+
+            TxtStatus.Text = average + " " + status;
+        }
+    }
+}
